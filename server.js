429bd08ce25ebc79385b70502d434e776867daf8
Николай Лабазов <Nickolaylabazov@yandex.ru>
2020-01-11 20:14:20 +0300

    11.01

--- server.js
+++ server.js
@@ -49,13 +49,13 @@ request({
   method: 'POST',
   url: 'https://nickolaylabazov.amocrm.ru/oauth2/access_token',
  
- form: {
+ form: JSON.stringify({
     "client_id": "3ee4d2d1-8272-49ca-b068-64a6068570b4",
     "client_secret": "1a6fXO3Pdsk43rhS3AMdZtVVfCmPoKuZTRBc0vSJh4nM9vhJJeOUFgizf6Tvic5m",
     "grant_type": "authorization_code",
     "code": "def50200318217f8af82d3c18d04c111895d8f4450080a957d2e08d0dc8fef0e3dcbfc6bc3a02f3e90d7b5eff6683f6771706b7dcee4e7a9d0d5606edb847fc3be53ed22afc330875b5c67cf65a1d4f16fadb3b0a3492673e79ae976c3c1a3b487e23456d2fabfcd20a1fff50b3f24f73660a0dff3956ef1976a469c8fe98ebd0de7bc41b2ec85ad34fa8d132a8cce350aaa2a335688e19f52fb58f9a04043a56b664442776049a248d114bf36fe281c57d4d6627ec0da027388803e168673435e27bc70984a98f2aab408667aa85147b4df8e71d39a34cf49ab819d746129c667c4d3d34b4f734d2bef791045e27b7922439641e98b7bb0d56b67fb7107a1d196d2ab9e76761b356123033e776d3bc229fc9dcb59816524b7e8680cdc4f0c6f16835fb79e68f148dde05dbf6329f0cf7d98355668eebe7dfbb0585cb2d388817886391e5b174b6d0abef129cc2d3c613d116ef1e6bb37a00b28d22bacee5363b66a7c1247e0b7e96276d568ac73360eaf70fc79ad03c88a344a3163956e1426766d57b2637fd32d5f9a472804f0c2816220d92d73bd8cf9dce39eaee36b8ba86af8855c1177c9cdbb700a6ea8d089d88fe5826f9d95fbfa6471ece1cb226f738bb64c3ef9a757ab",
    "redirect_uri": "https://crm-hook.herokuapp.com"
-  },  
+  }),  
   function (error, response, body) {
     console.log(response);
     if (!error && response.statusCode == 200) {
